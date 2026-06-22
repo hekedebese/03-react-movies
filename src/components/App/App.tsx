358b38f7ca91d0fa6acee9e6287c41a96a97b4a6
xenoterror <anohinnikita537@gmail.com>
2026-06-22 16:20:14 +0300

    toaster

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -4,7 +4,7 @@ import SearchBar from "../SearchBar/SearchBar";
 import { fetchMovies } from "../../services/movieService";
 import type { Movie } from "../../types/movie";
 import MovieGrid from "../MovieGrid/MovieGrid";
-import toast from "react-hot-toast";
+import { Toaster } from "react-hot-toast";
 import Loader from "../Loader/Loader";
 import ErrorMessage from "../ErrorMessage/ErrorMessage";
 
@@ -35,6 +35,7 @@ export default function App() {
   };
   return (
     <div className={css.app}>
+      <Toaster></Toaster>
       <SearchBar onSubmit={handleSearch}></SearchBar>
       {isLoading && <Loader></Loader>}
       {isError && <ErrorMessage></ErrorMessage>}
